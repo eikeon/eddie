88eb3df1b1f542d95a549fd89d1777f60d7ef9d4
Daniel Krech <eikeon@eikeon.com>
2013-10-09 17:41:37 -0400

    Rewrote as a Marvin Bit.

--- eddie.go
+++ eddie.go
@@ -1,63 +1,103 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
 	"os/signal"
+	"path"
+	"runtime"
+	"strings"
 	"sync/atomic"
 	"time"
+
+	"github.com/eikeon/gpio"
+	"github.com/eikeon/marvin/nog"
 )
 
-func doTransition(name string) {
-	host := "marvin.local"
-	values := url.Values{"do_transition": {name}}
-	if r, err := http.PostForm("http://"+host+"/post", values); err != nil {
-		log.Println(err)
-	} else {
-		log.Println("response:", r)
-	}
+var Root = ""
+
+func init() {
+	_, filename, _, _ := runtime.Caller(0)
+	Root = path.Dir(filename)
 }
 
-func main() {
-	log.Println("starting")
+type Eddie struct {
+	nog.InOut
+}
 
-	notifyChannel := make(chan os.Signal, 1)
-	signal.Notify(notifyChannel, os.Interrupt)
+func (e *Eddie) Run(in <-chan nog.Message, out chan<- nog.Message) {
+	options := nog.BitOptions{Name: "Eddie", Required: false}
+	if what, err := json.Marshal(&options); err == nil {
+		out <- nog.NewMessage("Eddie", string(what), "register")
+	} else {
+		log.Println("StateChanged err:", err)
+	}
 
-	ch := make(chan bool, 1)
-	go GPIOInterrupt(38, ch)
+	name := "eddie.html"
+	if j, err := os.OpenFile(path.Join(Root, name), os.O_RDONLY, 0666); err == nil {
+		if b, err := ioutil.ReadAll(j); err == nil {
+			out <- nog.NewMessage("Eddie", string(b), "template")
+		} else {
+			log.Println("ERROR reading:", err)
+		}
+	} else {
+		log.Println("WARNING: could not open ", name, err)
+	}
+
+	ch, err := gpio.GPIOInterrupt(38)
+	if err != nil {
+		panic(err)
+	}
 
 	var pressCount int64
 	for {
 		select {
+		case m := <-in:
+			if m.Why == "statechanged" {
+				dec := json.NewDecoder(strings.NewReader(m.What))
+				if err := dec.Decode(e); err != nil {
+					log.Println("eddie decode err:", err)
+				}
+			}
 		case value := <-ch:
 			if value {
+				out <- nog.NewMessage("Eddie", "hello", "eddie")
 				atomic.AddInt64(&pressCount, 1)
 				time.AfterFunc(time.Second, func() {
 					atomic.AddInt64(&pressCount, -1)
 				})
 				switch pressCount {
 				case 1:
-					doTransition("all off")
+					out <- nog.NewMessage("Eddie", "set light All to off", "Eddie")
 				case 2:
-					doTransition("all nightlight")
+					out <- nog.NewMessage("Eddie", "set light All to nightlight", "Eddie")
 				default:
-					doTransition("chime")
+					out <- nog.NewMessage("Eddie", "set light All to chime", "Eddie")
 				}
-			}
-		case sig := <-notifyChannel:
-			switch sig {
-			case os.Interrupt:
-				log.Println("handling:", sig)
-				goto Done
-			default:
-				log.Fatal("Unexpected Signal:", sig)
 			}
 		}
 	}
-Done:
+}
+
+func main() {
+	log.Println("starting")
+
+	// "ws://marvin.local:80/message", "", "http://marvin.local/"
+	go nog.RemoteAdd("ws://gato.local:9999/message", "", "http://marvin.local:9999/", &Eddie{})
+
+	notifyChannel := make(chan os.Signal, 1)
+	signal.Notify(notifyChannel, os.Interrupt)
+
+	sig := <-notifyChannel
+	switch sig {
+	case os.Interrupt:
+		log.Println("handling:", sig)
+	default:
+		log.Fatal("Unexpected Signal:", sig)
+	}
+
 	log.Println("stopping")
 
 }
